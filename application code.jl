b3661e852ad472d3f5298f22d2e728a37b43607f
Sahil Naik <nsahil992@gmail.com>
2023-09-22 15:35:26 +0530

    Initial commit

--- application code.jl
+++ application code.jl
@@ -1,2 +1,5 @@
-
-
+IceCreams = ["Chocolate", "Strawberry", "Vanilla", "ButterScotch", "Black current", "Choco Chip", "Mango"]
+Size = ["Small", "Medium", "Large"]
+medium = ["Cup", "Cone", "box"]
+Toppings = ["Oreo", "Sprinkles", "Nuts", "Whipped Cream", "Cereals"]
+while true
